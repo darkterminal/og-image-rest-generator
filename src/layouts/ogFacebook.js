294bd2e894c5b452806c9689785c3384a5c5071f
Imam Ali Mustofa <d.darkterminal@gmail.com>
2023-03-15 21:51:04 +0000

    refactor: facebook o:image and improvment

--- src/layouts/ogFacebook.js
+++ src/layouts/ogFacebook.js
@@ -3,14 +3,7 @@ const calculateFontSize = require("../utils/calculateFontSize");
 const shorten = require("../utils/shorten");
 const wrapText = require("../utils/wrapText");
 
-module.exports = async (
-  title,
-  author,
-  logo = "https://metaphore.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fmetaphor.ee0ad64a.webp&w=640&q=75",
-  head,
-  writer
-) => {
-  // Canvas size
+module.exports = async (title, author, logo, head, writer) => {
   const WIDTH = 1200;
   const HEIGHT = 630;
   const BORDER_SIZE = 20;
@@ -24,53 +17,37 @@ module.exports = async (
 
   ctx.lineWidth = BORDER_SIZE;
   ctx.strokeStyle = "#000000";
-  ctx.strokeRect(
-    BORDER_SIZE / 2,
-    BORDER_SIZE / 2,
-    WIDTH - BORDER_SIZE,
-    HEIGHT - BORDER_SIZE
-  );
+  ctx.strokeRect(BORDER_SIZE / 2, BORDER_SIZE / 2, WIDTH - BORDER_SIZE, HEIGHT - BORDER_SIZE);
 
   // Write Metaphor Story
   ctx.font = "25px Inter Medium";
   ctx.fillStyle = "#03001C";
   ctx.fillText(head, PADDING_LEFT, 120);
+
   // Draw underline
-  var text = ctx.measureText(head);
   ctx.strokeStyle = "#03001C";
   ctx.lineWidth = 5;
   ctx.beginPath();
   ctx.lineTo(PADDING_LEFT, 133);
-  ctx.lineTo(PADDING_LEFT + text.width, 133);
+  ctx.lineTo(PADDING_LEFT + ctx.measureText(head).width, 133);
   ctx.stroke();
 
   // Write Metaphor Story
   const titleLength = title.length;
   const yTitle = titleLength > 60 ? 40 : 60;
-  const shortTitle = shorten(title, 60);
+  const theTitle = shorten(title, 60);
 
-  ctx.font = `${calculateFontSize(
-    ctx,
-    shortTitle,
-    WIDTH - BORDER_SIZE * 2,
-    HEIGHT - BORDER_SIZE * 2,
-    65,
-    titleLength > 60 ? 75 : 95
-  )}px Inter ExtraBold`;
-  let wrappedText = wrapText(
-    ctx,
-    shortTitle,
-    PADDING_LEFT,
-    yTitle,
-    1080,
-    titleLength > 60 ? 80 : 90
-  );
+  const titleFontSize = calculateFontSize(ctx, theTitle, WIDTH - BORDER_SIZE * 2, HEIGHT - BORDER_SIZE * 2, 65, titleLength > 60 ? 75 : 95);
+  ctx.font = `${titleFontSize}px Inter ExtraBold`;
+  let wrappedText = wrapText(ctx, theTitle, PADDING_LEFT, yTitle, 1080, titleLength > 60 ? 80 : 90);
   let yAddition = 340;
+
   if (wrappedText[0].length > 3) {
     wrappedText[0].pop();
     yAddition = 340 + 40;
   }
-  wrappedText[0].forEach(function (item) {
+
+  wrappedText[0].forEach(item => {
     ctx.fillText(item[0], item[1], item[2] - wrappedText[1] + yAddition);
   });
 
@@ -78,38 +55,37 @@ module.exports = async (
   const img = await loadImage(`https://github.com/${author}.png`);
   const imgPat = ctx.createPattern(img, "no-repeat");
   const minRadius = Math.min(img.width, img.height) / 2;
+
   // get scale of circle image
   const scale = 50 / minRadius;
+
   // transform to put origin at top left of bounding rectangle scaling to fit image pattern
   ctx.setTransform(scale, 0, 0, scale, PADDING_LEFT, 470);
   ctx.fillStyle = imgPat;
   ctx.beginPath();
   ctx.arc(minRadius, minRadius, minRadius, 0, Math.PI * 2);
   ctx.fill();
+
   // reset the default transform
   ctx.setTransform(1, 0, 0, 1, 0, 0);
 
   // Write the story teller
-  ctx.font = `${20}px Inter ExtraBold`;
+  ctx.font = "20px Inter ExtraBold";
   ctx.fillStyle = "#03001C";
   ctx.fillText(`${writer}:`, 180, 500);
+
   // Write author name
-  ctx.font = `${35}px Inter ExtraBold`;
+  ctx.font = "35px Inter ExtraBold";
   ctx.fillStyle = "#03001C";
   ctx.fillText(`git@${author}`, 180, 540);
 
   const logoImage = await loadImage(logo);
-  const scaleFactor = Math.min(640 / logoImage.width, 440 / logoImage.height);
-  const scaledWidth = logoImage.width * scaleFactor;
-  const scaledHeight = logoImage.height * scaleFactor;
+  const scaleFactor = 0.3;
+  const logoWidth = logoImage.width * scaleFactor;
+  const logoHeight = logoImage.height * scaleFactor;
 
-  ctx.drawImage(
-    logoImage,
-    890,
-    460,
-    scaledWidth / 2 - 90,
-    scaledHeight / 2 - 90
-  );
+  // Draw Logo
+  ctx.drawImage(logoImage, WIDTH - BORDER_SIZE - (logoWidth + 45), HEIGHT - BORDER_SIZE - (logoHeight + 20), logoWidth, logoHeight);
 
   return canvas;
 };
--- src/utils/wrapText.js
+++ src/utils/wrapText.js
@@ -8,42 +8,41 @@
  * @param {Number} lineHeight the height of one line (as defined by us)
  * @returns Array
  */
-module.exports = function (ctx, text, x, y, maxWidth, lineHeight) {
-  // First, split the words by spaces
-  let words = text.split(" ");
-  // Then we'll make a few variables to store info about our line
-  let line = "";
-  let testLine = "";
-  // wordArray is what we'l' return, which will hold info on
-  // the line text, along with its x and y starting position
-  let wordArray = [];
-  // totalLineHeight will hold info on the line height
-  let totalLineHeight = 0;
+module.exports = function wrapText(ctx, text, x, y, maxWidth, lineHeight) {
+  const words = text.split(' ');
+  const lines = [];
+  let line = '';
 
-  // Next we iterate over each word
-  for (var n = 0; n < words.length; n++) {
-    // And test out its length
-    testLine += `${words[n]} `;
-    var metrics = ctx.measureText(testLine);
-    var testWidth = metrics.width;
-    // If it's too long, then we start a new line
-    if (testWidth > maxWidth && n > 0) {
-      wordArray.push([line, x, y]);
+  for (let i = 0; i < words.length; i++) {
+    const testLine = line + words[i] + ' ';
+    const metrics = ctx.measureText(testLine);
+    const testWidth = metrics.width;
+
+    if (testWidth > maxWidth) {
+      // truncate text if line is too long
+      let truncatedText;
+      if (line.length <= 60) {
+        truncatedText = line.trim() + ' ';
+      } else {
+        truncatedText = line.trim().substring(0, 60) + '...';
+      }
+      lines.push([truncatedText, x, y]);
+      line = words[i] + ' ';
       y += lineHeight;
-      totalLineHeight += lineHeight;
-      line = `${words[n]} `;
-      testLine = `${words[n]} `;
     } else {
-      // Otherwise we only have one line!
-      line += `${words[n]} `;
-    }
-    // Whenever all the words are done, we push whatever is left
-    if (n === words.length - 1) {
-      wordArray.push([line, x, y]);
+      line = testLine;
     }
   }
 
-  // And return the words in array, along with the total line height
-  // which will be (totalLines - 1) * lineHeight
-  return [wordArray, totalLineHeight];
+  // add the last line
+  const lastLine = line.trim();
+  if (lastLine) {
+    lines.push([lastLine, x, y]);
+  }
+
+  // calculate total line height
+  const totalLineHeight = (lines.length - 1) * lineHeight;
+
+  // return the words in array, along with the total line height
+  return [lines, totalLineHeight];
 };
